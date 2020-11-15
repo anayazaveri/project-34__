bb30d0381933688b48fb4885285c9e17091a24b5
anayazaveri <70576812+anayazaveri@users.noreply.github.com>
2020-11-15 15:33:35 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+//Create variables here
+
+var dog;
+var happyDog;
+var dataBase;
+var foodS;
+var foodStock;
+
+
+function preload()
+{
+  //load images here
+  
+  dogImg1 = loadImage("dogImg.png");
+  dogImg2 = loadImage("dogImg1.png");
+
+}
+
+function setup() {
+  var canvas = createCanvas(500,500);
+
+  
+
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
